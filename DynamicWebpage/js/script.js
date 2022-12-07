d5294fa6ed9694416f45619b4cf86d26e7444f29
Ethan <118588535+00NAhte@users.noreply.github.com>
2022-12-07 14:55:36 -0700

    Add files via upload

---
+++ DynamicWebpage/js/script.js
@@ -0,0 +1,200 @@
+var ScavR1 = false
+var ScavR2 = false
+var ScavR3 = false
+var ScavR4 = false
+var ScavR5 = false
+var ScavR6 = false
+var ScavR7 = false
+var ScavR8 = false
+var ScavR9 = false
+var ScavR10 = false
+var ScavR11 = false
+var ScavR12 = false
+var ScavR13 = false
+var ScavR14 = false
+var ScavY1 = false
+var ScavY2 = false
+var ScavY3 = false
+var ScavY4 = false
+var ScavY5 = false
+var ScavG1 = false
+
+$(document).ready(function () {
+    $("#R1").click(function () {
+        $("#R1").css("opacity", '0.0');
+    },
+        function () {
+            $("#R1").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R2").click(function () {
+        $("#R2").css("opacity", '0.0');
+    },
+        function () {
+            $("#R2").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R3").click(function () {
+        $("#R3").css("opacity", '0.0');
+    },
+        function () {
+            $("#R3").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R4").click(function () {
+        $("#R4").css("opacity", '0.0');
+    },
+        function () {
+            $("#R4").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R5").click(function () {
+        $("#R5").css("opacity", '0.0');
+    },
+        function () {
+            $("#R5").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R6").click(function () {
+        $("#R6").css("opacity", '0.0');
+    },
+        function () {
+            $("#R6").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R7").click(function () {
+        $("#R7").css("opacity", '0.0');
+    },
+        function () {
+            $("#R7").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R8").click(function () {
+        $("#R8").css("opacity", '0.0');
+    },
+        function () {
+            $("#R8").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R9").click(function () {
+        $("#R9").css("opacity", '0.0');
+    },
+        function () {
+            $("#R9").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R10").click(function () {
+        $("#R10").css("opacity", '0.0');
+    },
+        function () {
+            $("#R10").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R11").click(function () {
+        $("#R11").css("opacity", '0.0');
+    },
+        function () {
+            $("#R11").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R12").click(function () {
+        $("#R12").css("opacity", '0.0');
+    },
+        function () {
+            $("#R12").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R13").click(function () {
+        $("#R13").css("opacity", '0.0');
+    },
+        function () {
+            $("#R13").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#R14").click(function () {
+        $("#R14").css("opacity", '0.0');
+    },
+        function () {
+            $("#R14").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#Y1").click(function () {
+        $("#Y1").css("opacity", '0.0');
+    },
+        function () {
+            $("#Y1").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#Y2").click(function () {
+        $("#Y2").css("opacity", '0.0');
+    },
+        function () {
+            $("#Y2").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#Y3").click(function () {
+        $("#Y3").css("opacity", '0.0');
+    },
+        function () {
+            $("#Y3").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#Y4").click(function () {
+        $("#Y4").css("opacity", '0.0');
+    },
+        function () {
+            $("#Y4").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#Y5").click(function () {
+        $("#Y5").css("opacity", '0.0');
+    },
+        function () {
+            $("#Y5").css('opacity', '1.0')
+        });
+});
+
+$(document).ready(function () {
+    $("#G1").click(function () {
+        $("#G1").css("opacity", '0.0');
+    },
+        function () {
+            $("#G1").css('opacity', '1.0')
+        });
+});
